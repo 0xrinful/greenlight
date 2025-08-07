b779f306d4af624ba4e9de1f843a532e0a3f6816
0xrinful <0xrinful@gmail.com>
2025-08-07 18:41:07 +0300

    Chapter 7 (CRUD Operations)

--- cmd/api/handlers_movies.go
+++ cmd/api/handlers_movies.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
-	"time"
 
 	"greenlight/internal/data"
 	"greenlight/internal/validator"
@@ -16,13 +16,15 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	movie := data.Movie{
-		ID:        id,
-		CreatedAt: time.Now(),
-		Title:     "Casablanca",
-		Runtime:   102,
-		Genres:    []string{"drama", "romance", "war"},
-		Version:   1,
+	movie, err := app.models.Movies.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.sendNotFoundError(w, r)
+		default:
+			app.sendServerError(w, r, err)
+		}
+		return
 	}
 
 	err = app.renderJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
@@ -58,5 +60,95 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
-	fmt.Fprintf(w, "%+v\n", input)
+	err = app.models.Movies.Insert(movie)
+	if err != nil {
+		app.sendServerError(w, r, err)
+		return
+	}
+
+	headers := make(http.Header)
+	headers.Set("Location", fmt.Sprintf("/v1/movies/%d", movie.ID))
+
+	err = app.renderJSON(w, http.StatusCreated, envelope{"movie": movie}, headers)
+	if err != nil {
+		app.sendServerError(w, r, err)
+	}
+}
+
+func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.sendNotFoundError(w, r)
+		return
+	}
+
+	movie, err := app.models.Movies.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.sendNotFoundError(w, r)
+		default:
+			app.sendServerError(w, r, err)
+		}
+		return
+	}
+
+	var input struct {
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
+	}
+
+	err = app.decodeJSON(w, r, &input)
+	if err != nil {
+		app.sendServerError(w, r, err)
+		return
+	}
+
+	movie.Title = input.Title
+	movie.Year = input.Year
+	movie.Runtime = input.Runtime
+	movie.Genres = input.Genres
+
+	v := validator.New()
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.sendValidationError(w, r, v.Errors)
+		return
+	}
+
+	err = app.models.Movies.Update(movie)
+	if err != nil {
+		app.sendServerError(w, r, err)
+		return
+	}
+
+	err = app.renderJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.sendServerError(w, r, err)
+	}
+}
+
+func (app *application) deleteMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.sendNotFoundError(w, r)
+		return
+	}
+
+	err = app.models.Movies.Delete(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.sendNotFoundError(w, r)
+		default:
+			app.sendServerError(w, r, err)
+		}
+		return
+	}
+
+	err = app.renderJSON(w, http.StatusOK, envelope{"message": "movie successfully deleted"}, nil)
+	if err != nil {
+		app.sendServerError(w, r, err)
+	}
 }
